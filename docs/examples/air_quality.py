6fa0e1d6c9dac262e36b963ac3d417ce7bd014de
Jacob <jacob.dink@strong.io>
2021-05-25 09:51:03 -0500

    Remove kernelspecs

--- docs/examples/air_quality.py
+++ docs/examples/air_quality.py
@@ -8,10 +8,6 @@
 #       format_name: light
 #       format_version: '1.5'
 #       jupytext_version: 1.6.0
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
 # ---
 
 # + {"nbsphinx": "hidden"}
