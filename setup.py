1c40d7d6288082b5f24215bb8b5a40bbf682b8d2
Jacob <jacob.dink@strong.io>
2023-05-18 15:38:27 -0500

    scipy required

--- setup.py
+++ setup.py
@@ -10,8 +10,9 @@ setup(
     zip_safe=False,
     install_requires=[
         'backports.cached-property',
-        'torch>=1.8',
-        'numpy>=1.4'
+        'torch>=1.12',
+        'numpy>=1.4',
+        'scipy>=1.10'
     ],
     extras_require={
         'tests': ['parameterized>=0.7', 'filterpy>=1.4', 'pandas>=1.0'],
