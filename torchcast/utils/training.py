90e1f6795aa5d11fb32061608276cd25c745247a
Jacob <jacob.dink@strong.io>
2024-10-07 21:37:09 -0400

    forward_kwargs -> kwargs_getter

--- torchcast/utils/training.py
+++ torchcast/utils/training.py
@@ -1,3 +1,11 @@
+"""
+These are simple training classes for PyTorch models, with specialized subclasses for torchcast's model-classes (i.e.,
+when the data are too big for the :func:`torchcast.state_space.StateSpaceModel.fit()` method). Additionaly, there is a
+special class for training neural networks to embed complex seasonal patterns into lower dimensional embeddings.
+
+While the classes in this module are helpful for quick development, they are not necessarily meant to replace more
+sophisticated tools (e.g. PyTorch Lightning) in more complex settings.
+"""
 import warnings
 from itertools import zip_longest
 
@@ -6,7 +14,7 @@ import torch
 from torch.utils.data import DataLoader, Dataset
 from torch.optim import Optimizer
 import torch.nn as nn
-from typing import Generator, Union, Type, Sequence, Tuple
+from typing import Generator, Union, Type, Sequence, Tuple, Dict, Optional
 
 from tqdm.auto import tqdm
 
@@ -17,17 +25,18 @@ from torchcast.utils.features import fourier_model_mat
 
 
 class BaseTrainer:
-    """
-    Base class; child classes need to implement `_get_closure`
-    """
+    _device = None
 
     def __init__(self,
                  module: nn.Module,
                  optimizer: Union[Optimizer, Type[Optimizer]] = torch.optim.Adam):
         self.module = module
-        self.optimizer = optimizer if isinstance(optimizer, Optimizer) else optimizer(module.parameters())
-        self._device = None
+        if isinstance(optimizer, type):
+            optimizer = optimizer([p for p in module.parameters() if p.requires_grad])
+        self.optimizer = optimizer
+
         self.to(torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
+
         self.epoch = 0
 
     def to(self, device) -> 'BaseTrainer':
@@ -85,10 +94,7 @@ class SimpleTrainer(BaseTrainer):
                  optimizer: Union[Optimizer, Type[Optimizer]] = torch.optim.Adam,
                  loss_fn: callable = torch.nn.MSELoss()):
         self.loss_fn = loss_fn
-        super().__init__(
-            module=module,
-            optimizer=optimizer
-        )
+        super().__init__(module=module, optimizer=optimizer)
 
     def _get_closure(self, batch: Dataset) -> callable:
         inputs, targets, *_other = batch
@@ -115,36 +121,54 @@ class SimpleTrainer(BaseTrainer):
 class StateSpaceTrainer(BaseTrainer):
     """
     TODO
+
+    :param module: A :class:`torchcast.state_space.StateSpaceModel` instance (e.g. ``KalmanFilter`` or ``ExpSmoother``).
+    :param kwargs_getter: If a sequence of strings are passed, these map the ``tensors[1:]`` of each
+     :class:`torchcast.utils.TimeSeriesDataset` to the corresponding keyword arguments of the module's ``forward``
+     method. If left unspecified, will pass ``tensor[1]`` as ``X`` (if the batch has such a tensor). You can also pass
+     a callable that takes the :class:`torchcast.utils.TimeSeriesDataset` and returns a dictionary that will be passed
+     as forward-kwargs.
+    :param optimizer: An optimizer or a class to instantiate an optimizer
     """
 
     def __init__(self,
                  module: nn.Module,
-                 forward_kwargs: Sequence[str] = (),
+                 kwargs_getter: Optional[Sequence[str]] = None,
                  optimizer: Union[Optimizer, Type[Optimizer]] = torch.optim.Adam):
-        self.forward_kwargs = forward_kwargs
-        super().__init__(
-            module=module,
-            optimizer=optimizer
-        )
+
+        self.kwargs_getter = kwargs_getter
+        super().__init__(module=module, optimizer=optimizer)
 
     def get_loss(self, pred: Predictions, y: torch.Tensor) -> torch.Tensor:
         return -pred.log_prob(y).mean()
 
-    def _get_closure(self, batch: TimeSeriesDataset) -> callable:
+    def _getXy(self, batch: TimeSeriesDataset) -> Tuple[torch.Tensor, Dict[str, torch.Tensor]]:
         y = batch.tensors[0]
-        kwargs = {}
-        for i, (k, t) in enumerate(zip_longest(self.forward_kwargs, batch.tensors[1:])):
-            if k is None:
-                raise RuntimeError(
-                    f"Found element-{i + 1} of the dataset.tensors, but `forward_kwargs` doesn't have enough "
-                    f"elements: {self.forward_kwargs}"
-                )
-            if t is None:
-                raise RuntimeError(
-                    f"Found element-{i} of `forward_kwargs`, but `dataset.tensors` doesn't have enough "
-                    f"elements: {batch}"
-                )
-            kwargs[k] = t
+
+        if callable(self.kwargs_getter):
+            kwargs = self.kwargs_getter(batch)
+        else:
+            if self.kwargs_getter is None:
+                self.kwargs_getter = ['X'] if len(batch.tensors) > 1 else []
+
+            kwargs = {}
+            for i, (k, t) in enumerate(zip_longest(self.kwargs_getter, batch.tensors[1:])):
+                if k is None:
+                    raise RuntimeError(
+                        f"Found element-{i + 1} of the dataset.tensors, but `kwargs_getter` doesn't have enough "
+                        f"elements: {self.kwargs_getter}"
+                    )
+                if t is None:
+                    raise RuntimeError(
+                        f"Found element-{i} of `kwargs_getter`, but `dataset.tensors` doesn't have enough "
+                        f"elements: {batch}"
+                    )
+                kwargs[k] = t
+
+        return y, kwargs
+
+    def _get_closure(self, batch: TimeSeriesDataset) -> callable:
+        y, kwargs = self._getXy(batch)
 
         def closure():
             self.optimizer.zero_grad()
@@ -161,15 +185,15 @@ class StateSpaceTrainer(BaseTrainer):
 
 class SeasonalEmbeddingsTrainer(BaseTrainer):
     """
-    TODO
+    This trainer is designed to train a :class:`torch.nn.Module` with
     """
 
     # TODO: classmethod `from_dt_unit` or something that puts in decent defaults for hourly, daily, weekly data
 
     def __init__(self,
                  module: nn.Module,
-                 weekly: int,
                  yearly: int,
+                 weekly: int,
                  daily: int,
                  other: Sequence[Tuple[np.timedelta64, int]] = (),
                  loss_fn: callable = torch.nn.MSELoss(),
@@ -182,6 +206,22 @@ class SeasonalEmbeddingsTrainer(BaseTrainer):
         self.loss_fn = loss_fn
 
         self._warned = False
+
+    def times_to_arrays(self, times: np.ndarray) -> Dict[str, torch.Tensor]:
+        out = {}
+        if self.yearly:
+            out['yearly'] = torch.as_tensor(fourier_model_mat(times, K=self.yearly, period='yearly'))
+        if self.weekly:
+            out['weekly'] = torch.as_tensor(fourier_model_mat(times, K=self.weekly, period='weekly'))
+        if self.daily:
+            out['daily'] = torch.as_tensor(fourier_model_mat(times, K=self.daily, period='daily'))
+        for period, k in self.other:
+            out[str(period)] = torch.as_tensor(fourier_model_mat(times, K=k, period=period))
+        return out
+
+    def times_to_model_mat(self, times: np.ndarray) -> torch.Tensor:
+        arrays = list(self.times_to_arrays(times).values())
+        return torch.cat(arrays, -1)
 
     def get_loss(self, predictions: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
         nan_mask = torch.isnan(y)
@@ -224,15 +264,3 @@ class SeasonalEmbeddingsTrainer(BaseTrainer):
         X, y = self._getXy(batch)
         emb = self.module(X)
         return LinearModel.solve_and_predict(y=y, X=emb)
-
-    def times_to_model_mat(self, times: np.ndarray) -> torch.Tensor:
-        out = []
-        if self.yearly:
-            out.append(torch.as_tensor(fourier_model_mat(times, K=self.yearly, period='yearly')))
-        if self.weekly:
-            out.append(torch.as_tensor(fourier_model_mat(times, K=self.weekly, period='weekly')))
-        if self.daily:
-            out.append(torch.as_tensor(fourier_model_mat(times, K=self.daily, period='daily')))
-        for period, k in self.other:
-            out.append(torch.as_tensor(fourier_model_mat(times, K=k, period=period)))
-        return torch.cat(out, -1)
