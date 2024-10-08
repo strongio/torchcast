3fe6faaa24adc86d9f1ca7a6c3b2eb414092e973
Jacob <jacob.dink@strong.io>
2024-10-07 21:37:56 -0400

    fix bug in predictions.to_dataframe

--- torchcast/state_space/predictions.py
+++ torchcast/state_space/predictions.py
@@ -379,6 +379,11 @@ class Predictions(nn.Module):
             dataset = self.dataset_metadata.copy()
             if dataset.group_names is None:
                 dataset.group_names = [f"group_{i}" for i in range(self.num_groups)]
+            if dataset.start_offsets.dtype.name.startswith('datetime') and not dataset.dt_unit:
+                raise ValueError(
+                    "Unable to infer `dt_unit`, please call ``predictions.set_metadata(dt_unit=X)``, or pass `dataset` "
+                    "to ``predictions.to_dataframe()``"
+                )
         else:
             for measure_group, tensor in zip(dataset.measures, dataset.tensors):
                 for i, measure in enumerate(measure_group):
